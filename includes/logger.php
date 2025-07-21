18f05048150de70ef0a1cd52f15ae4ecdddca227
KENTAROTK <oktk33@gmail.com>
2025-07-21 13:48:16 +0900

    Delete includes directory

--- includes/logger.php
+++
@@ -1,8 +0,0 @@
-<?php
-function tcl_log_message($message) {
-    $upload_dir = wp_upload_dir();
-    $log_file = trailingslashit($upload_dir['basedir']) . 'topic-cluster-log.txt';
-    $datetime = date('Y-m-d H:i:s');
-    $entry = "[" . $datetime . "] " . print_r($message, true) . "\n";
-    file_put_contents($log_file, $entry, FILE_APPEND);
-}
